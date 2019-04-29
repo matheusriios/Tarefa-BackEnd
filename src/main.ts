cd27d5d352ccb44bda29156acccf3cba286fff6f
matheus.rios <matheus.rios@zrp.com.br>
2019-04-28 21:57:15 -0300

    update cors application

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
+
+//options for cors midddleware
+const options: cors.CorsOptions = {
+  allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token"],  
+  methods: "GET, HEAD, OPTIONS, PUT, PATCH, POST, DELETE",
+  origin: "*",
+};
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule); 
-  await app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-    next();
-  }) 
+  const app = await NestFactory.create(AppModule);
+  await app.use(cors(options));
   await app.listen(3000);
 }
+
 bootstrap();
